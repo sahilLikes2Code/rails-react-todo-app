511fbc53f9f2daa6ca35277af5a80d535b83036e
Sahil Kapoor <sahil.kapoor440@gmail.com>
2022-01-18 13:11:23 +0530

    Added login feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 
+import { either, isEmpty, isNil } from "ramda";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
+import Login from "components/Authentication/Login";
 import Signup from "components/Authentication/Signup";
+import PrivateRoute from "components/Common/PrivateRoute";
 import Dashboard from "components/Dashboard";
 import PageLoader from "components/PageLoader";
 import CreateTask from "components/Tasks/CreateTask";
 import EditTask from "components/Tasks/EditTask";
 import ShowTask from "components/Tasks/ShowTask";
+import { getFromLocalStorage } from "helpers/storage";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const authToken = getFromLocalStorage("authToken");
+  const isLoggedIn = !either(isNil, isEmpty)(authToken);
 
   useEffect(() => {
     initializeLogger();
@@ -35,6 +41,13 @@ const App = () => {
         <Route exact path="/tasks/create" component={CreateTask} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute
+          path="/"
+          redirectRoute="/login"
+          condition={isLoggedIn}
+          component={Dashboard}
+        />
       </Switch>
     </Router>
   );
--- app/javascript/src/components/NavBar/index.jsx
+++ app/javascript/src/components/NavBar/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { getFromLocalStorage } from "helpers/storage";
+
 import NavItem from "./NavItem";
 
 const NavBar = () => {
+  const userName = getFromLocalStorage("authUserName");
+
   return (
     <nav className="bg-white shadow">
       <div className="px-2 mx-auto max-w-7xl sm:px-4 lg:px-8">
@@ -17,10 +21,14 @@ const NavBar = () => {
               />
             </div>
           </div>
-          <div className="flex justify-end items-center">
-            <a className="inline-flex items-center px-1 pt-1 text-sm font-semibold leading-5 text-opacity-50 border-b-2 border-transparent transition duration-150 ease-in-out cursor-pointer text-bb-gray-600 hover:text-bb-gray-600 focus:outline-none focus:text-bb-gray-700">
-              LogOut
-            </a>
+          <div className="flex items-center justify-end gap-x-4">
+            <span
+              className="inline-flex items-center px-2 pt-1 text-sm font-regular leading-5 text-bb-gray-600
+              text-opacity-50 transition duration-150 ease-in-out border-b-2 border-transparent focus:outline-none
+              focus:text-bb-gray-700"
+            >
+              {userName}
+            </span>
           </div>
         </div>
       </div>
