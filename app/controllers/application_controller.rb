31d33fc45d7fabd861c1224c578619788d7ac383
Sahil Kapoor <sahil.kapoor440@gmail.com>
2022-01-19 18:11:47 +0530

    Moved pundit helpers to Authorizable concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include Pundit
-  rescue_from Pundit::NotAuthorizedError, with: :handle_authorization_error
+  include Authorizable
 
   def authenticate_user_using_x_auth_token
-    # debugger
     user_email = request.headers["X-Auth-Email"]
     auth_token = request.headers["X-Auth-Token"].presence
     user = user_email && User.find_by_email(user_email)
@@ -25,9 +23,5 @@ class ApplicationController < ActionController::Base
 
     def current_user
       @current_user
-    end
-
-    def handle_authorization_error
-      render status: :forbidden, json: { error: t("authorization.denied") }
     end
 end
