8ccc437d05202f8476ff129a91a9c51658997a2c
Sahil Kapoor <sahil.kapoor440@gmail.com>
2022-01-19 12:05:11 +0530

    Added controller tests and dowgrade bundler version

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,16 @@ class ActiveSupport::TestCase
   include ActionView::Helpers::TranslationHelper
   include FactoryBot::Syntax::Methods
   # Run tests in parallel with specified workers
+  # previous code
+
+  def headers(user, options = {})
+    {
+      Accept: "application/json",
+      "Content_Type" => "application/json",
+      "X-Auth-Token" => user.authentication_token,
+      "X-Auth-Email" => user.email
+    }.merge(options)
+  end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
